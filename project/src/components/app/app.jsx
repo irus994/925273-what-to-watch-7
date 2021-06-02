fc9e92659ea25f864d730991a05f77339346c12c
Ириная <irus994@gmail.com>
2021-06-02 23:22:33 +0300

    Создание первых компонентов: основного и карточки, добавление им переменных, отрисовка

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
 import General from '../general/general.jsx';
+import PropTypes from 'prop-types';
 
+export default function App(props) {
+  const {filmsCount, filmName, genre, year} = props;
 
-function App() {
-  return <General/>;
+  return (
+    <General
+      filmsCount={filmsCount}
+      filmName={filmName}
+      genre={genre}
+      year={year}
+    />
+  );
 }
 
-export default App;
+App.propTypes = {
+  filmsCount: PropTypes.array.isRequired,
+  filmName: PropTypes.string.isRequired,
+  genre: PropTypes.string.isRequired,
+  year: PropTypes.number.isRequired,
+};
+
+
