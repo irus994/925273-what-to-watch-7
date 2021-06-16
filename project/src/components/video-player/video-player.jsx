b5500c12e9461001d31dab7cf3a5d155699428af
Ириная <irus994@gmail.com>
2021-06-16 19:48:20 +0300

    добавление превью с проигрывающимся видео

---
+++ project/src/components/video-player/video-player.jsx
@@ -0,0 +1,30 @@
+import React, {useEffect, useRef} from 'react';
+import PropTypes from 'prop-types';
+
+export default function VideoPlayer(props) {
+  const {prevPoster, video, isActive} = props;
+
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    if (isActive) {
+      setTimeout(() => {
+        videoRef.current.play();
+      }, 1000);
+      return;
+    }
+    videoRef.current.pause();
+  }, [isActive]);
+
+  return (
+    <div className="small-film-card__image">
+      <video muted src={`${video}`} ref={videoRef} poster={`/img/${prevPoster}`} width="280" height="175"/>
+    </div>
+  );
+}
+
+VideoPlayer.propTypes = {
+  prevPoster: PropTypes.string.isRequired,
+  video: PropTypes.string.isRequired,
+  isActive: PropTypes.bool.isRequired,
+};
